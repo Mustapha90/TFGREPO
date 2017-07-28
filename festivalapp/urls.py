e5a0cc93563077d5204da8b95b427ee5f4f9ff86
Mustapha <mj4ever001@gmail.com>
2017-07-28 23:13:04 +0200

    add

--- festivalapp/urls.py
+++ festivalapp/urls.py
@@ -6,5 +6,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^contacto/$', views.contacto, name='contacto'),
 ]
 
--- festivalapp/views.py
+++ festivalapp/views.py
@@ -14,3 +14,6 @@ def index(request):
 
 
 
+def contacto(request):
+	return render(request, 'festivalapp/contact-us.html')
+
