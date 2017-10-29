40d74df20b0aa773f993e8f3aacc9c4cc4bd97f4
Mustapha <mj4ever001@gmail.com>
2017-10-29 15:39:22 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash          
 
-heroku apps:create --region eu
+heroku apps:create --region eu --app tfg1617
 heroku addons:create heroku-postgresql:hobby-dev
 heroku config:set ON_HEROKU=1
 heroku config:set SECRET_KEY=`openssl rand -base64 32`
 git push heroku master
-heroku run python manage.py makemigrations festivalapp --noinput
-heroku run python manage.py makemigrations --noinput
-heroku run python manage.py migrate --noinput
-heroku open
+#heroku run python manage.py makemigrations festivalapp --noinput
+#heroku run python manage.py makemigrations --noinput
+#heroku run python manage.py migrate --noinput
+#heroku open
 
