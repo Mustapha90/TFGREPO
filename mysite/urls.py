ff5bbd782d2421d30185b7a30891fa62a740f810
Mustapha <mj4ever001@gmail.com>
2017-10-29 15:17:18 +0100

    deploy

--- mysite/urls.py
+++ mysite/urls.py
@@ -31,8 +31,8 @@ urlpatterns = [
     url(r'^$', RedirectView.as_view(url='/festivalapp/')),
     url(r'^festivalapp/', include('festivalapp.urls')),
     url(r'^admin/', admin.site.urls),
-   # url(r'^login/$', auth_views.login,{'extra_context':{'datos': getdatos()}}, name='login'),
     url(r'^login/$', auth_views.login, name='login'),
+    #url(r'^login/$', auth_views.login,{'extra_context':{'datos': getdatos()}}, name='login'),
     url(r'^accounts/password/change/$', password_change, {
         'template_name': 'registration/password_change_form.html'}, 
         name='password_change'),
